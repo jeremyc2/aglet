a8c7c7b8b75cf42d0729ed77707dc2a62a15ecdf
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2022-09-22 14:20:39 -0500

    update ts.d file

--- types/components/color-square/color-square.d.ts
+++ types/components/color-square/color-square.d.ts
@@ -2,7 +2,7 @@ import { LitElement } from "lit";
 export declare class AGColorSquare extends LitElement {
     name: string;
     color: string;
-    format: string;
+    format: "hex" | "rgb" | "hsl";
     static styles: import("lit").CSSResult;
     render(): import("lit").TemplateResult<1>;
 }
