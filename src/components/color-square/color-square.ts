ee751105ad0388244a4f27aae88b538d963c9cb2
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2022-09-22 10:44:13 -0500

    added initial content to color-square

--- src/components/color-square/color-square.ts
+++ src/components/color-square/color-square.ts
@@ -1,5 +1,19 @@
 import { css, html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { colord } from "colord";
+
+function convertColorCode(colorCode: string, format: "hex" | "rgb" | "hsl") {
+  if (format === "hex") {
+    return colord(colorCode).toHex().toUpperCase();
+  }
+  if (format === "rgb") {
+    return colord(colorCode).toRgbString();
+  }
+  if (format === "hsl") {
+    return colord(colorCode).toHslString();
+  }
+  throw "Unsupported Color Format";
+}
 
 @customElement("ag-color-square")
 export class AGColorSquare extends LitElement {
@@ -10,7 +24,7 @@ export class AGColorSquare extends LitElement {
   color: string;
 
   @property()
-  format: string;
+  format: "hex" | "rgb" | "hsl";
 
   static styles = css`
     @tailwind base;
@@ -24,9 +38,10 @@ export class AGColorSquare extends LitElement {
   render() {
     return html`<div
         class="rounded shadow bg-gray-3 cursor-copy h-24 hover:scale-90"
+        style="background-color: ${convertColorCode(this.color, this.format)};"
       ></div>
       <div class="relative">
-        <div></div>
+        <div>${this.name}</div>
         <div class="text-gray-500 font-extralight"></div>
       </div>`;
   }
